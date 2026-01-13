c01675a99fb7d1fcf17fc6e761d8e9b139580307
abdul razak <razak.velozion@gmail.com>
2026-01-13 23:08:56 +0800

    changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://finance-tracker-backend-vi7z.onrender.com/api';
 
 // Create axios instance with auth token
 const api = axios.create({
